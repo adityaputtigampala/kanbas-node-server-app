c241d5f7c8b6f0bae6db3336c2a4ca63251ca543
adityaputtigampala <aditya.puttigampala@gmail.com>
2024-07-16 16:37:53 -0700

    add database placeholder

---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,244 @@
+export default[
+    {
+      "_id": "M101",
+      "name": "Introduction to Rocket Propulsion",
+      "description": "Basic principles of rocket propulsion and rocket engines.",
+      "course": "RS101",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "History of Rocketry",
+          "description": "A brief history of rocketry and space exploration.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "Rocket Propulsion Fundamentals",
+          "description": "Basic principles of rocket propulsion.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Rocket Engine Types",
+          "description": "Overview of different types of rocket engines.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M102",
+      "name": "Fuel and Combustion",
+      "description": "Understanding rocket fuel, combustion processes, and efficiency.",
+      "course": "RS101",
+      "lessons": [
+        {
+          "_id": "L201",
+          "name": "Rocket Fuel",
+          "description": "Overview of different types of rocket fuels.",
+          "module": "M102"
+        },
+        {
+          "_id": "L202",
+          "name": "Combustion Processes",
+          "description": "Understanding combustion processes and efficiency.",
+          "module": "M102"
+        },
+        {
+          "_id": "L203",
+          "name": "Combustion Instability",
+          "description": "Understanding combustion instability and mitigation.",
+          "module": "M102"
+        }
+      ]
+    },
+    {
+      "_id": "M103",
+      "name": "Nozzle Design",
+      "description": "Principles of rocket nozzle design and performance optimization.",
+      "course": "RS101",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Nozzle Design",
+          "description": "Overview of different types of rocket nozzles.",
+          "module": "M103"
+        },
+        {
+          "_id": "L302",
+          "name": "Nozzle Performance",
+          "description": "Understanding nozzle performance and efficiency.",
+          "module": "M103"
+        },
+        {
+          "_id": "L303",
+          "name": "Nozzle Optimization",
+          "description": "Optimizing nozzle design for specific applications.",
+          "module": "M103"
+        }
+      ]
+    },
+    {
+      "_id": "M201",
+      "name": "Basics of Aerodynamics",
+      "description": "Introduction to the fundamental concepts of aerodynamics, including airflow, pressure dynamics, and basic fluid mechanics.",
+      "course": "RS102",
+      "lessons": [
+        {
+          "_id": "L401",
+          "name": "Introduction to Fluid Dynamics",
+          "description": "Covers the principles of fluid dynamics that form the basis of aerodynamic studies."
+        },
+        {
+          "_id": "L402",
+          "name": "Pressure and Flow Visualization",
+          "description": "Techniques for visualizing airflow and pressure changes around various shapes and objects."
+        }
+      ]
+    },
+    {
+      "_id": "M202",
+      "name": "Advanced Aerodynamic Theories",
+      "description": "Delves into more complex aerodynamic theories, including compressible flows and advanced computational methods.",
+      "course": "RS102",
+      "lessons": [
+        {
+          "_id": "L501",
+          "name": "Compressible Flow",
+          "description": "Exploration of aerodynamics in conditions where air compressibility plays a significant role, such as in supersonic flight."
+        },
+        {
+          "_id": "L502",
+          "name": "Computational Fluid Dynamics (CFD)",
+          "description": "Introduction to CFD techniques used to simulate and analyze fluid flows."
+        }
+      ]
+    },
+    {
+      "_id": "M203",
+      "name": "Aerodynamics in Practice",
+      "description": "Application of aerodynamic principles in real-world scenarios, focusing on aircraft and automotive design.",
+      "course": "RS102",
+      "lessons": [
+        {
+          "_id": "L601",
+          "name": "Aerodynamics in Aircraft Design",
+          "description": "Study of how aerodynamics influences the design and performance of aircraft."
+        },
+        {
+          "_id": "L602",
+          "name": "Aerodynamics in Automotive Engineering",
+          "description": "Examination of the role of aerodynamics in the design and efficiency of automobiles."
+        }
+      ]
+    },
+    {
+      "_id": "M301",
+      "name": "Introduction to Spacecraft Systems",
+      "description": "This module covers the basics of spacecraft systems including structural components, navigation, and life support.",
+      "course": "RS103",
+      "lessons": [
+        {
+          "_id": "L701",
+          "name": "Spacecraft Structures",
+          "description": "Overview of the materials and design principles used in constructing spacecraft."
+        },
+        {
+          "_id": "L702",
+          "name": "Spacecraft Navigation Systems",
+          "description": "Examination of the technologies used for navigation and control in space."
+        }
+      ]
+    },
+    {
+      "_id": "M302",
+      "name": "Propulsion Systems",
+      "description": "Detailed study of the different types of propulsion systems used in space travel, including chemical and electric propulsion.",
+      "course": "RS103",
+      "lessons": [
+        {
+          "_id": "L801",
+          "name": "Chemical Propulsion",
+          "description": "Lorem Ipsum."
+        },
+        {
+          "_id": "L802",
+          "name": "Electric Propulsion",
+          "description": "Lorem Ipsum"
+        }
+      ]
+    },
+    {
+      "_id": "M303",
+      "name": "Spacecraft Design Lorem Ipsum",
+      "description": "Lorem Ipsum",
+      "course": "RS103",
+      "lessons": [
+        {
+          "_id": "L901",
+          "name": "Design for Lorem Ipsum",
+          "description": "Lorem Ipsum"
+        },
+        {
+          "_id": "L902",
+          "name": "Lorem Ipsum Lorem Ipsum in Spacecraft Design",
+          "description": "Lorem Ipsum"
+        }
+      ]
+    },
+    {
+      "_id": "M401",
+      "name": "Foundations of Lorem Ipsum Chemistry",
+      "description": "Lorem Ipsum",
+      "course": "RS104",
+      "lessons": [
+        {
+          "_id": "L1001",
+          "name": "Lorem Ipsum Structures and Hybridization",
+          "description": "Lorem Ipsum"
+        },
+        {
+          "_id": "L1002",
+          "name": "Lorem Ipsum",
+          "description": "Lorem Ipsum"
+        }
+      ]
+    },
+    {
+      "_id": "M402",
+      "name": "Lorem Ipsum",
+      "description": "Lorem Ipsum",
+      "course": "RS104",
+      "lessons": [
+        {
+          "_id": "L1101",
+          "name": "Lorem Ipsum Lorem Ipsum",
+          "description": "Lorem Ipsum"
+        },
+        {
+          "_id": "L1102",
+          "name": "Lorem Ipsum",
+          "description": "Lorem Ipsum."
+        }
+      ]
+    },
+    {
+      "_id": "M403",
+      "name": "Lorem Ipsum",
+      "description": "Lorem Ipsum",
+      "course": "RS104",
+      "lessons": [
+        {
+          "_id": "L1201",
+          "name": "Lorem Ipsum",
+          "description": "Lorem Ipsum."
+        },
+        {
+          "_id": "L1202",
+          "name": "Lorem Ipsum",
+          "description": "Lorem Ipsum"
+        }
+      ]
+    }
+  
+    
+  ];
